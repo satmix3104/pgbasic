418be5c49c61cebd9dfcbebef23f13c654f9124b
sat-mix3104 <sat-mix@outlook.jp>
2022-07-16 12:18:10 +0900

    gradle環境を作成

---
+++ JsonSort/lib/src/main/java/jp/co/wiss1/jsonsort/Main.java
@@ -0,0 +1,12 @@
+package jp.co.wiss1.jsonsort;
+
+/**
+ * Jsonソートアプリ
+ * @author
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
