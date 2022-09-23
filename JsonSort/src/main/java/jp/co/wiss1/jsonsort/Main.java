a7f21572d4cdb691015570b5442b2855aa404360
LAPTOP-IF0BD8UC\sat-m <sat-mix@outlook.jp>
2022-09-23 18:28:30 +0900

    HelloWorld追加 1

--- JsonSort/src/main/java/jp/co/wiss1/jsonsort/Main.java
+++ JsonSort/src/main/java/jp/co/wiss1/jsonsort/Main.java
@@ -8,5 +8,6 @@ package jp.co.wiss1.jsonsort;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
