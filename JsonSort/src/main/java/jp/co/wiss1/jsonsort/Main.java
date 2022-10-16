a2b72e13a5330211cab1d21a0af760232f73a13f
LAPTOP-IF0BD8UC\sat-m <sat-mix@outlook.jp>
2022-10-16 10:47:42 +0900

    ８に変更

--- JsonSort/src/main/java/jp/co/wiss1/jsonsort/Main.java
+++ JsonSort/src/main/java/jp/co/wiss1/jsonsort/Main.java
@@ -8,6 +8,6 @@ package jp.co.wiss1.jsonsort;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World7");
+        System.out.println("Hello World8");
     }
 }
